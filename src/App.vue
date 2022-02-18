de46875dbd0f1cf7f36ff64a816260c7d03cffec
max-tsogt <max@maxtsogt.com>
2022-02-18 23:59:32 +0900

    update eslint

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
+  <page-home />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
+import PageHome from './components/PageHome'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
     HelloWorld,
-  },
-};
+    PageHome
+  }
+}
 </script>
 
 <style>
---
+++ src/components/PageHome.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Hello from Page Home</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
