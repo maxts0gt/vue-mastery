18055f649a76a506575ef74014b87bb2954d8587
max-tsogt <max@maxtsogt.com>
2022-02-19 22:29:26 +0900

    add global styles

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
-  <page-home />
+  <div class="container">
+    <page-home />
+  </div>
 </template>
 
 <script>
-import PageHome from './components/PageHome'
+import PageHome from '@/components/PageHome'
 
 export default {
   name: 'App',
