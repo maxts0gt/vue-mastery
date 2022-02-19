c60ba5ec9d0366491194514283f1d5de68d98a42
max-tsogt <bazarsadtsogtjargal@gmail.com>
2022-02-19 22:55:15 +0900

    add vue router

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,16 @@
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
   <div class="container">
-    <page-home />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import PageHome from '@/components/PageHome'
 
 export default {
   name: 'App',
   components: {
-    PageHome
+
   }
 }
 </script>
